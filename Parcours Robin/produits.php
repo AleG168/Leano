51542ec265e1c341d6be463ebaadff74df27c512
AleG168 <aleguern.ledantec@gmail.com>
2025-06-11 14:20:27 +0200

    MAJ

---
+++ Parcours Robin/produits.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>LeanoDesign - Réalisations</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/navbar_Index.css">
+    <link rel="stylesheet" href="css/realisations.css">
+    <link rel="stylesheet" href="css/footer.css">
+</head>
+<body>
+
+<script src="https://cdn.jsdelivr.net/npm/aos@2.3.1/dist/aos.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
--- Parcours Robin/realisations.php
+++ Parcours Robin/realisations.php
@@ -21,70 +21,71 @@
                 <!-- Bloc 1 -->
                 <div class="col-lg-3 col-md-3 col-6 d-flex flex-column align-items-center realisation-block">
                     <a href="produits.php" class="square-block">
-                        <img src="assets/images/realisation1.jpg" alt="Réalisation 1" class="block-image">
+                        <img src="assets/Realisations/Aeronautique.png" alt="Réalisation 1" class="block-image">
+                        <div class="hover-text">Aéronautique</div>
                     </a>
-                    <span class="block-label mt-2">aaaaaaaa</span>
                 </div>
                 
                 <!-- Bloc 2 -->
                 <div class="col-lg-3 col-md-3 col-6 d-flex flex-column align-items-center realisation-block">
                     <a href="produits.php" class="square-block">
-                        <img src="assets/images/realisation2.jpg" alt="Réalisation 2" class="block-image">
+                        <img src="assets/Realisations/DefenseSecuritéCivileNaval.png" alt="Réalisation 2" class="block-image">
+                        <div class="hover-text">Défense Sécurité civile, Naval</div>
                     </a>
-                    <span class="block-label mt-2">bbbbbbbb</span>
                 </div>
                 
                 <!-- Bloc 3 -->
                 <div class="col-lg-3 col-md-3 col-6 d-flex flex-column align-items-center realisation-block">
                     <a href="produits.php" class="square-block">
-                        <img src="assets/images/realisation3.jpg" alt="Réalisation 3" class="block-image">
+                        <img src="assets/Realisations/Energie_RessourcesNaturelles.png" alt="Réalisation 3" class="block-image">
+                        <div class="hover-text">Énergie & ressources naturelles</div>
                     </a>
-                    <span class="block-label mt-2">cccccccc</span>
                 </div>
                 
                 <!-- Bloc 4 -->
                 <div class="col-lg-3 col-md-3 col-6 d-flex flex-column align-items-center realisation-block">
                     <a href="produits.php" class="square-block">
-                        <img src="assets/images/realisation4.jpg" alt="Réalisation 4" class="block-image">
+                        <img src="assets/Realisations/IndustrieEquipementIndustrielAgricole.png" alt="Réalisation 4" class="block-image">
+                        <div class="hover-text">Industrie, Équipement industriel, Agricole</div>
                     </a>
-                    <span class="block-label mt-2">dddddddd</span>
                 </div>
                 
                 <!-- Bloc 5 -->
                 <div class="col-lg-3 col-md-3 col-6 d-flex flex-column align-items-center realisation-block">
                     <a href="produits.php" class="square-block">
-                        <img src="assets/images/realisation5.jpg" alt="Réalisation 5" class="block-image">
+                        <img src="assets/Realisations/Nautisme.png" alt="Réalisation 5" class="block-image">
+                        <div class="hover-text">Nautisme</div>
                     </a>
-                    <span class="block-label mt-2">eeeeeeee</span>
                 </div>
                 
                 <!-- Bloc 6 -->
                 <div class="col-lg-3 col-md-3 col-6 d-flex flex-column align-items-center realisation-block">
                     <a href="produits.php" class="square-block">
-                        <img src="assets/images/realisation6.jpg" alt="Réalisation 6" class="block-image">
+                        <img src="assets/Realisations/SanteEquipementDomestique&BienEtre.png" alt="Réalisation 6" class="block-image">
+                        <div class="hover-text">Santé, Equipement domestique & Bien-être</div>
                     </a>
-                    <span class="block-label mt-2">ffffffff</span>
                 </div>
                 
                 <!-- Bloc 7 -->
                 <div class="col-lg-3 col-md-3 col-6 d-flex flex-column align-items-center realisation-block">
                     <a href="produits.php" class="square-block">
-                        <img src="assets/images/realisation7.jpg" alt="Réalisation 7" class="block-image">
+                        <img src="assets/Realisations/TelecomMultimédia.png" alt="Réalisation 7" class="block-image">
+                        <div class="hover-text">Télécom & Multimédia</div>
                     </a>
-                    <span class="block-label mt-2">gggggggg</span>
                 </div>
                 
                 <!-- Bloc 8 -->
                 <div class="col-lg-3 col-md-3 col-6 d-flex flex-column align-items-center realisation-block">
                     <a href="produits.php" class="square-block">
-                        <img src="assets/images/realisation8.jpg" alt="Réalisation 8" class="block-image">
+                        <img src="assets/Realisations/TransportMobilite.png" alt="Réalisation 8" class="block-image">
+                        <div class="hover-text">Transport & Mobilité</div>
                     </a>
-                    <span class="block-label mt-2">hhhhhhhh</span>
                 </div>
             </div>
         </div>
     </div>
-
+    <?php include 'footer.php'; ?>
+    
     <script src="https://cdn.jsdelivr.net/npm/aos@2.3.1/dist/aos.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
