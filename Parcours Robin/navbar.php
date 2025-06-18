cbc5a9b88892e7d5f7a2a380d2b2b6d12f1d372f
rnft781 <rnft78@gmail.com>
2025-06-18 14:42:24 +0200

    NavBarre

--- Parcours Robin/navbar.php
+++ Parcours Robin/navbar.php
@@ -1,40 +1,44 @@
-<!-- BOOTSTRAP NAVBAR -->
+
 <nav class="navbar navbar-expand-lg navbar-light bg-white py-3">
   <div class="container-fluid">
-    <div class="container d-flex justify-content-between align-items-center">
-      <div class="d-flex align-items-center me-2">
-        <!-- Logo à gauche (non cliquable) -->
-        <img src="assets/images/logo.png" alt="Leano Design Logo" style="height:80px; margin-left: -53px;" class="">
-        <a class="nav-link fs-4" href="index.php" title="Accueil">🏠</a>
+    <div class="container d-flex align-items-center justify-content-between">
+      <div class="d-flex align-items-center me-auto logo-wrapper">
+
+        <img src="assets/images/logo.png" alt="Logo de Leano Design" class="logo">
+
       </div>
-      <!-- Hamburger -->
+      
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <!-- Menu -->
-      <div class="collapse navbar-collapse justify-content-center" id="mainNavbar">  
-        <ul class="navbar-nav mb-2 mb-lg-0 border border-light rounded-0 px-4 py-2 shadow text-dark">
+      
+      <div class="collapse navbar-collapse justify-content-center flex-grow-1 nav-links-wrapper" id="mainNavbar">
+        
+        <a class="nav-link nav-icon" href="index.php" title="Accueil"><img src="assets/images/Home.png" alt="Accueil" style="height: 25px; vertical-align: middle;"></a>
+        <ul class="navbar-nav mb-2 mb-lg-0 border border-light rounded-0 px-4 py-2  text-dark mx-2">
           <li class="nav-item">
             <a class="nav-link fw-bold" href="historique.php" style="color: #000;">A Propos</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link fw-bold" href="prestations" style="color: #000;">Prestations</a>
+            <a class="nav-link fw-bold" href="prestation.php" style="color: #000;">Prestations</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link fw-bold" href="lieux-intervention.php" style="color: #000;">Démarches</a>
+            <a class="nav-link fw-bold" href="demarches.php" style="color: #000;">Démarches</a>
           </li>
           <li class="nav-item">
             <a class="nav-link fw-bold" href="realisations.php" style="color: #000;">Réalisations</a>
           </li>
         </ul>
+        
+        <a href="contact.php" class="nav-icon ms-2" aria-label="Contact" title="Contact"><img src="assets/images/Contact.png" alt="Contact" style="height: 25px; vertical-align: middle;"></a>
       </div>
-      <!-- Icônes à droite -->
-      <div class="d-flex align-items-center gap-3" style="margin-right: -53px;">
-        <a href="contact.php" class="fs-3" aria-label="Contact" title="Contact">✉️</a>
+      
+      <div class="d-flex align-items-center gap-3 ms-auto social-icons-wrapper">        
         <a href="https://www.instagram.com/leanodesign?igsh=M2Fkd3Q3Mm9vbzU3" target="_blank" class="fs-4 text-dark"><i class="fab fa-instagram"></i></a>
         <a href="https://fr.linkedin.com/company/leano-design" target="_blank" class="fs-4 text-dark"><i class="fab fa-linkedin-in"></i></a>
       </div>
     </div>
   </div>
 </nav>
-<!-- /BOOTSTRAP NAVBAR -->
+
+
